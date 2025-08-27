996206bcc757142d1782d359c2c32cddfcb59b91
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-27 20:51:00 -0300

    Comecamos a ver random , exemplo m01 e exercicio 08 feitos. o 10 ainda nao esta acabado

---
+++ src/Exercicio10.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Random rd = new Random();
+        int[] x = new int[10];
+        //preenchimento e impressao antes da ordenação
+        System.out.println("Antes da ordenação");
+        for (int i = 0; i < x.length; i++) {
+            x[i] = rd.nextInt(5,89);
+            System.out.print(x[i]+" ");
+        }
+
+        // lógica para ordenar (crescente)
+
+
+        System.out.println("\nDepois da ordenação");
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i]+" ");
+        }
+
+
+    }
+}
